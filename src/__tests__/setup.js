3a05a94c220c31be7c4a03f6129bc49826ffbb68
kashik09 <kashiku789@gmail.com>
2026-02-07 17:37:31 +0300

    test(ui): stub matchMedia for jsdom
    Provide a window.matchMedia mock in vitest setup to unblock ThemeContext in tests.

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -4,6 +4,19 @@ import '@testing-library/jest-dom/vitest'
 import 'whatwg-fetch' // Required for CodeGrade
 import { Blob } from 'node:buffer' // Fix for Node 16 issues
 
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  })
+}
+
 afterEach(() => {
   cleanup()
 })
